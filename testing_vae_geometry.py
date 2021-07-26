f9b55c952f7c62b9ab771c51b7d3ca5771964642
Miguel Gonzalez Duque <migd@itu.dk>
2021-07-26 11:07:09 +0200

    tests in a circle

--- testing_vae_geometry.py
+++ testing_vae_geometry.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 from vae_geometry import VAEGeometry
 
-model_name = "mariovae_z_dim_2_only_playable_epoch_480"
+model_name = "mariovae_z_dim_2_overfitting_epoch_480"
 vae = VAEGeometry()
 vae.load_state_dict(torch.load(f"models_experiment/{model_name}.pt"))
 vae.update_cluster_centers(model_name, False, beta=-1.5)
