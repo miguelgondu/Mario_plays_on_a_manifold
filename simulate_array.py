6e5cbb2b4e9c53171105f65d9120621e51c4078b
Miguel González Duque <miguelgondu@gmail.com>
2021-09-29 16:32:02 +0200

    Adds the possibility to repeat a level n times

--- simulate_array.py
+++ simulate_array.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import multiprocessing as mp
+from typing import Dict, List
 
 import click
 import numpy as np
@@ -11,7 +12,8 @@ from simulator import test_level_from_int_array
 @click.command()
 @click.argument("array_path", type=str)
 @click.option("--processes", type=int, default=5)
-def simulate_array(array_path, processes):
+@click.option("--repetitions_per_level", type=int, default=1)
+def simulate_array(array_path, processes, repetitions_per_level):
     """
     Takes an array stored as an .npz with
     the keys "zs" and "levels" and simulates it,
@@ -24,6 +26,10 @@ def simulate_array(array_path, processes):
     array = np.load(array_path)
     levels = array["levels"]
     zs = array["zs"]
+
+    # Repeat
+    levels = np.repeat(levels, repetitions_per_level, axis=0)
+    zs = np.repeat(zs, repetitions_per_level, axis=0)
 
     with mp.Pool(processes) as p:
         results = p.map(test_level_from_int_array, levels)
