bf2440ddb62e4a0f03b1cd64c38cc791fcd1164a
Miguel González Duque <miguelgondu@gmail.com>
2021-11-04 10:17:53 +0100

    Changes the ground truth to save an array instead

--- ground_truth_experiment.py
+++ ground_truth_experiment.py
@@ -2,13 +2,14 @@
 This experiment illuminates the latent space
 by trying the decoded level several times using
 Baumgarten's A* agent.
+
+It creates an array that is can then simulated by
+simulate_array.py
 """
 
-import os
 import json
 from pathlib import Path
 from multiprocessing import Pool
-from itertools import repeat
 
 import click
 import torch
@@ -18,57 +19,69 @@ from simulator import test_level_from_z
 from vae_mario_hierarchical import VAEMarioHierarchical
 
 
-def process(i, z):
-    """
-    This function simulates the level
-    that the VAE generates from z.
-    """
-    vae = VAEMarioHierarchical(device="cpu")
-    vae.load_state_dict(torch.load(f"./models/another_vae_final.pt"))
-    vae.eval()
-    print(f"Testing {z} (index {i})")
+# def process(i, z):
+#     """
+#     This function simulates the level
+#     that the VAE generates from z.
+#     """
+#     print(f"Testing {z} (index {i})")
 
-    # Create the folder for the data
-    cwd = Path(".")
-    path_to_exp_folder = cwd / "data" / "ground_truth" / "another_vae_final"
-    path_to_exp_folder.mkdir(parents=True, exist_ok=True)
+#     # Create the folder for the data
+#     cwd = Path(".")
+#     path_to_exp_folder = cwd / "data" / "ground_truth" / "another_vae_final"
+#     path_to_exp_folder.mkdir(parents=True, exist_ok=True)
 
-    # Test the level 5 times
-    for iteration in range(5):
-        res = test_level_from_z(z, vae)
-        thing_to_save = {
-            "model_name": "another_vae_final",
-            "z": tuple([float(zi) for zi in z.detach().numpy()]),
-            "iteration": iteration,
-            **res,
-        }
+#     # Test the level 5 times
+#     for iteration in range(5):
+#         res = test_level_from_z(z, vae)
+#         thing_to_save = {
+#             "model_name": "another_vae_final",
+#             "z": tuple([float(zi) for zi in z.detach().numpy()]),
+#             "iteration": iteration,
+#             **res,
+#         }
 
-        path_to_exp = path_to_exp_folder / f"z_{i:05d}_{iteration}.json"
-        with open(path_to_exp, "w") as fp:
-            json.dump(thing_to_save, fp)
+#         path_to_exp = path_to_exp_folder / f"z_{i:05d}_{iteration}.json"
+#         with open(path_to_exp, "w") as fp:
+#             json.dump(thing_to_save, fp)
 
-    print(f"Processed z {z}.")
+#     print(f"Processed z {z}.")
 
 
 @click.command()
-@click.option("--processes", type=int, default=5)
-def main(processes):
+@click.option("--model-name", type=str, default="another_vae_final")
+@click.option("--n-samples", type=int, default=5)
+def main(model_name, n_samples):
+    vae = VAEMarioHierarchical(device="cpu")
+    vae.load_state_dict(torch.load(f"./models/{model_name}.pt"))
+    vae.eval()
+
     x_lims = [-5, 5]
     y_lims = [-5, 5]
 
-    n_rows = 50
-    n_cols = 50
-    z1 = np.linspace(*x_lims, n_cols)
-    z2 = np.linspace(*y_lims, n_rows)
+    n_grid = 50
+    z1 = np.linspace(*x_lims, n_grid)
+    z2 = np.linspace(*y_lims, n_grid)
 
     zs = torch.Tensor([[a, b] for a in reversed(z1) for b in z2])
+    cat = vae.decode(zs)
 
-    print(f"Starting the Pool.")
-    with Pool(processes) as p:
-        p.starmap(
-            process,
-            zip(range(len(zs)), zs),
-        )
+    levels = cat.sample((n_samples,)).detach().numpy()
+    levels = levels.reshape(n_grid * n_grid * n_samples, *levels.shape[2:])
+
+    zs = zs.detach().numpy()
+    zs = np.repeat(zs, n_samples, axis=0)
+
+    print(f"zs: {zs.shape}")
+    print(f"levels: {levels.shape}")
+    assert zs.shape[0] == levels.shape[0]
+
+    print(f"Array saved for {model_name}.")
+    np.savez(
+        f"./data/arrays/ground_truth_{model_name}.npz",
+        zs=zs,
+        levels=levels,
+    )
 
 
 if __name__ == "__main__":
