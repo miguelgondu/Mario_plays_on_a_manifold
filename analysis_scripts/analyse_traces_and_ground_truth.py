f5cc3719908e7d1526400d5bea4a479f2df75a1b
Miguel González Duque <miguelgondu@gmail.com>
2022-01-18 15:26:59 +0100

    Makes videos for analysing kernel hyperparameters

--- analysis_scripts/analyse_traces_and_ground_truth.py
+++ analysis_scripts/analyse_traces_and_ground_truth.py
@@ -1,4 +1,5 @@
 from itertools import product
+from pathlib import Path
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,47 +7,62 @@ from sklearn.gaussian_process import GaussianProcessClassifier
 
 from evolving_playability import get_ground_truth
 
+traces = Path("./data/evolution_traces").glob("trace_*.npz")
 
-array = np.load("./data/evolution_traces/bigger_trace.npz")
-zs = array["zs"]
-playabilities = array["playabilities"]
+for f in traces:
+    array = np.load(f)
+    all_zs = array["zs"]
+    all_playabilities = array["playabilities"]
 
-gpc = GaussianProcessClassifier()
-gpc.fit(zs, playabilities)
+    gt_img = get_ground_truth()
+    for m in range(500):
+        # print(str(f), len(zs))
+        print(f"{f.name}, {m:03d}/500", flush=True, end="\r")
+        zs = all_zs[: 100 + m]
+        playabilities = all_playabilities[: 100 + m]
 
-z1s = np.linspace(-5, 5, 50)
-z2s = np.linspace(-5, 5, 50)
+        gpc = GaussianProcessClassifier()
+        gpc.fit(zs, playabilities)
 
-bigger_grid = np.array([[z1, z2] for z1, z2 in product(z1s, z2s)])
-res, var = gpc.predict_proba(bigger_grid, return_var=True)
-predictions = gpc.predict(bigger_grid)
+        z1s = np.linspace(-5, 5, 50)
+        z2s = np.linspace(-5, 5, 50)
 
-p_dict = {(z[0], z[1]): r[1] for z, r in zip(bigger_grid, res)}
-var_dict = {(z[0], z[1]): v for z, v in zip(bigger_grid, var)}
-pred_dict = {(z[0], z[1]): pred for z, pred in zip(bigger_grid, predictions)}
+        bigger_grid = np.array([[z1, z2] for z1, z2 in product(z1s, z2s)])
+        res, var = gpc.predict_proba(bigger_grid, return_var=True)
+        predictions = gpc.predict(bigger_grid)
 
-positions = {
-    (x, y): (i, j) for j, x in enumerate(z1s) for i, y in enumerate(reversed(z2s))
-}
+        p_dict = {(z[0], z[1]): r[1] for z, r in zip(bigger_grid, res)}
+        var_dict = {(z[0], z[1]): v for z, v in zip(bigger_grid, var)}
+        pred_dict = {(z[0], z[1]): pred for z, pred in zip(bigger_grid, predictions)}
 
-var_img = np.zeros((len(z2s), len(z1s)))
-for z, (i, j) in positions.items():
-    var_img[i, j] = var_dict[z]
+        positions = {
+            (x, y): (i, j)
+            for j, x in enumerate(z1s)
+            for i, y in enumerate(reversed(z2s))
+        }
 
-p_img = np.zeros((len(z2s), len(z1s)))
-for z, (i, j) in positions.items():
-    p_img[i, j] = p_dict[z]
+        var_img = np.zeros((len(z2s), len(z1s)))
+        for z, (i, j) in positions.items():
+            var_img[i, j] = var_dict[z]
 
-pred_img = np.zeros((len(z2s), len(z1s)))
-for z, (i, j) in positions.items():
-    pred_img[i, j] = pred_dict[z]
+        p_img = np.zeros((len(z2s), len(z1s)))
+        for z, (i, j) in positions.items():
+            p_img[i, j] = p_dict[z]
 
-_, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(7 * 2, 7 * 2))
+        pred_img = np.zeros((len(z2s), len(z1s)))
+        for z, (i, j) in positions.items():
+            pred_img[i, j] = pred_dict[z]
 
-gt_img = get_ground_truth()
-ax1.imshow(gt_img, extent=[-5, 5, -5, 5], cmap="Blues", vmin=0.0, vmax=1.0)
-ax2.imshow(pred_img, extent=[-5, 5, -5, 5], cmap="Blues", vmin=0.0, vmax=1.0)
-ax3.imshow(p_img, extent=[-5, 5, -5, 5], cmap="Blues", vmin=0.0, vmax=1.0)
-ax3.scatter(zs[:, 0], zs[:, 1], c=playabilities, cmap="Wistia", vmin=0.0, vmax=1.0)
-ax4.imshow(var_img, extent=[-5, 5, -5, 5], cmap="Blues", vmin=0.0, vmax=1.0)
-plt.show()
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(7 * 2, 7 * 2))
+
+        plt.title(f"{f.name}, m={m:03d}")
+        ax1.imshow(gt_img, extent=[-5, 5, -5, 5], cmap="Blues", vmin=0.0, vmax=1.0)
+        ax2.imshow(p_img, extent=[-5, 5, -5, 5], cmap="Blues", vmin=0.0, vmax=1.0)
+        ax2.scatter(
+            zs[:, 0], zs[:, 1], c=playabilities, cmap="Wistia", vmin=0.0, vmax=1.0
+        )
+        fig.tight_layout()
+        fig.savefig(
+            f"./data/plots/evolving_playability/for_videos/{m:03d}_{f.name.replace('.npz', '.png')}"
+        )
+        plt.close("all")
