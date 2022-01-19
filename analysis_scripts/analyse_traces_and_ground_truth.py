1ec57772e48cf54490300cc704b3ba416191f3d6
Miguel González Duque <miguelgondu@gmail.com>
2022-01-19 16:18:25 +0100

    Gets videos up to 300 for all traces

--- analysis_scripts/analyse_traces_and_ground_truth.py
+++ analysis_scripts/analyse_traces_and_ground_truth.py
@@ -6,6 +6,7 @@ import torch as t
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import WhiteKernel, Matern
 
 from vae_mario_hierarchical import VAEMarioHierarchical
 
@@ -95,34 +96,41 @@ def plot_all_grids():
         fig.savefig(save_at)
 
 
+def verify_length_of_all_traces():
+    traces = Path("./data/evolution_traces/five_vaes").glob("*.npz")
+    for path in traces:
+        zs, _ = load_trace(path)
+        print(path, len(zs))
+
+
 def save_video_for_all_traces():
-    traces = Path("./data/evolution_traces").glob("trace_*.npz")
+    traces = Path("./data/evolution_traces/five_vaes").glob("*.npz")
+    videos_path = Path("./data/plots/five_vaes/AL_for_videos")
+    videos_path.mkdir(exist_ok=True, parents=True)
 
     for f in traces:
-        all_zs, all_playabilities = load_trace(f)
+        if "id_1" in f.name:
+            continue
 
-        gt_img = get_ground_truth()
-        for m in range(500):
+        all_zs, all_playabilities = load_trace(f)
+        m_iters = min(len(all_zs) - 100, 300)
+        for m in range(m_iters):
             # print(str(f), len(zs))
-            print(f"{f.name}, {m:03d}/500", flush=True, end="\r")
+            print(f"{f.name}, {m:03d}/{m_iters}", flush=True, end="\r")
             zs = all_zs[: 100 + m]
             playabilities = all_playabilities[: 100 + m]
 
-            gpc = GaussianProcessClassifier()
+            kernel = 1.0 * Matern(nu=3 / 2) + 1.0 * WhiteKernel()
+            gpc = GaussianProcessClassifier(kernel=kernel)
             gpc.fit(zs, playabilities)
 
             z1s = np.linspace(-5, 5, 50)
             z2s = np.linspace(-5, 5, 50)
 
             bigger_grid = np.array([[z1, z2] for z1, z2 in product(z1s, z2s)])
-            res, var = gpc.predict_proba(bigger_grid, return_var=True)
-            predictions = gpc.predict(bigger_grid)
+            res = gpc.predict_proba(bigger_grid)
 
             p_dict = {(z[0], z[1]): r[1] for z, r in zip(bigger_grid, res)}
-            var_dict = {(z[0], z[1]): v for z, v in zip(bigger_grid, var)}
-            pred_dict = {
-                (z[0], z[1]): pred for z, pred in zip(bigger_grid, predictions)
-            }
 
             positions = {
                 (x, y): (i, j)
@@ -130,33 +138,27 @@ def save_video_for_all_traces():
                 for i, y in enumerate(reversed(z2s))
             }
 
-            var_img = np.zeros((len(z2s), len(z1s)))
-            for z, (i, j) in positions.items():
-                var_img[i, j] = var_dict[z]
-
             p_img = np.zeros((len(z2s), len(z1s)))
             for z, (i, j) in positions.items():
                 p_img[i, j] = p_dict[z]
 
-            pred_img = np.zeros((len(z2s), len(z1s)))
-            for z, (i, j) in positions.items():
-                pred_img[i, j] = pred_dict[z]
+            fig, ax = plt.subplots(1, 1, figsize=(7 * 1, 7 * 1))
 
-            fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(7 * 2, 7 * 2))
-
-            plt.title(f"{f.name}, m={m:03d}")
-            ax1.imshow(gt_img, extent=[-5, 5, -5, 5], cmap="Blues", vmin=0.0, vmax=1.0)
-            ax2.imshow(p_img, extent=[-5, 5, -5, 5], cmap="Blues", vmin=0.0, vmax=1.0)
-            ax2.scatter(
+            plt.title(f"{f.name}, m={m:04d}")
+            ax.imshow(p_img, extent=[-5, 5, -5, 5], cmap="Blues", vmin=0.0, vmax=1.0)
+            ax.scatter(
                 zs[:, 0], zs[:, 1], c=playabilities, cmap="Wistia", vmin=0.0, vmax=1.0
             )
             fig.tight_layout()
-            fig.savefig(
-                f"./data/plots/evolving_playability/for_videos/{m:03d}_{f.name.replace('.npz', '.png')}"
-            )
+            fig.savefig(videos_path / f"{m:04d}_{f.name.replace('.npz', '.png')}")
             plt.close("all")
+
+            if m == m_iters - 1:
+                print()
 
 
 if __name__ == "__main__":
-    plot_all_ground_truths()
-    plot_all_grids()
+    # plot_all_ground_truths()
+    # plot_all_grids()
+    # verify_length_of_all_traces()
+    save_video_for_all_traces()
